c8dea2bf360bf0473bbf22afbca22bf7c20953e3
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-09-06 15:47:17 +0300

    Creating Models

---
+++ TutsPlus-SwiftReader/ArticleModel.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleModel.swift
+//  TutsPlus-SwiftReader
+//
+//  Created by Yan Malinovsky on 06.09.16.
+//  Copyright © 2016 Yan Malinovsky. All rights reserved.
+//
+
+import UIKit
+
+class ArticleModel: NSObject {
+    var title: String = String()
+    var link: String = String()
+    var pubData: String = String()
+}
---
+++ TutsPlus-SwiftReader/FeedModel.swift
@@ -0,0 +1,16 @@
+//
+//  FeedModel.swift
+//  TutsPlus-SwiftReader
+//
+//  Created by Yan Malinovsky on 06.09.16.
+//  Copyright © 2016 Yan Malinovsky. All rights reserved.
+//
+
+import UIKit
+
+class FeedModel: NSObject {
+    var title: String = String()
+    var url: String = String()
+    var articles: [ArticleModel] = [ArticleModel]()
+    
+}
