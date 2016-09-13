48a56bfe514665591cdd1ce88ab8eb441460e464
Yan Malinovsky <yan.malinovsky@gmail.com>
2016-09-13 16:35:01 +0300

    Presenting the Feeds

--- TutsPlus-SwiftReader/FeedsTableViewController.swift
+++ TutsPlus-SwiftReader/FeedsTableViewController.swift
@@ -105,23 +105,24 @@ class FeedsTableViewController: UITableViewController, NSXMLParserDelegate {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return feeds.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("FeedCell", forIndexPath: indexPath)
 
-        // Configure the cell...
+        let feed: FeedModel = feeds[indexPath.row]
+        cell.textLabel!.text = feed.title
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
